c082dd6ed55c976eb467ebe6122042a3cee200ab
kwhsiung <kaiwenhsiung@gmail.com>
2020-08-24 19:26:07 +0800

    Modifications for Sites with a Custom Server

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,5 +1,9 @@
 module.exports = {
   ci: {
+    collect: {
+      url: ['http://localhost:3000/'],
+      startServerCommand: 'rails server -e production',
+    },
     upload: {
       target: 'temporary-public-storage',
     },
